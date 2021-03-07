32e18d7ca301bc0cc38f18de70776e5f49e01e9a
AASIF RAZA <aasifraza9123@gmail.com>
2021-03-07 15:59:16 +0530

    created maven project

---
+++ DemoHib/src/test/java/com/aasif/DemoHib/Alien.java
@@ -0,0 +1,30 @@
+package com.aasif.DemoHib;
+
+public class Alien {
+		private int aid;
+		private String aname;
+		private String color;
+		public int getAid() {
+			return aid;
+		}
+		public void setAid(int aid) {
+			this.aid = aid;
+		}
+		public String getAname() {
+			return aname;
+		}
+		public void setAname(String aname) {
+			this.aname = aname;
+		}
+		public String getColor() {
+			return color;
+		}
+		public void setColor(String color) {
+			this.color = color;
+		}
+		@Override
+		public String toString() {
+			return "Alien [aid=" + aid + ", aname=" + aname + ", color=" + color + "]";
+		}
+	
+}
---
+++ DemoHib/src/test/java/com/aasif/DemoHib/App.java
@@ -0,0 +1,11 @@
+package com.aasif.DemoHib;
+
+public class App {
+ public static void main(String[] args) {
+	Alien a = new Alien();
+	a.setAid(101);
+	a.setAname("Aasif");
+	a.setColor("black");
+	
+}
+}
